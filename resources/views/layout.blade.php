6270ca330d23bd4c2114136f64aec56f3bbb71e4
pivnati <natanmuleta911@gmail.com>
2024-10-02 15:05:49 -0700

    make the col-12

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,6 +12,7 @@
 <body>
     <div class="container">
         <div class="row">
+            <div class="col-md-12">
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
                     <a class="navbar-brand" href="#">Navbar</a>
@@ -47,6 +48,7 @@
                             <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                             <button class="btn btn-outline-success" type="submit">Search</button>
                         </form>
+                        </div>
                     </div>
                 </div>
             </nav>
