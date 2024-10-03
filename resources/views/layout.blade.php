72dca32c233110974d3ed8a28f224c07e9665b4d
pivnati <natanmuleta911@gmail.com>
2024-10-03 16:16:12 -0700

    modify the layout by removing the three div inside it

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -98,9 +98,9 @@ div.content {
 
             </div>
             <div class="col-md-9">
-                <div class="content">
+               
                 @yield('content')
-                </div>
+                
             </div>
         </div>
 
--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -1,9 +1,7 @@
 @extends('layout')
 @section('content')
-    <div class="container">
-        <div class="row">
- 
-            <div class="col-md-9">
+    
+           
                 <div class="card">
                     <div class="card-header">
                         <h2>Student Application</h2>
@@ -60,7 +58,5 @@
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </div>
+       
 @endsection
