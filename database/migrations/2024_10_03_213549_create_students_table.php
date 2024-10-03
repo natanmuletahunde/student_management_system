18ae70d54b1364a09a873ba6e729fde0554e48b6
pivnati <natanmuleta911@gmail.com>
2024-10-03 14:40:30 -0700

    add the name, address,mobile

--- database/migrations/2024_10_03_213549_create_students_table.php
+++ database/migrations/2024_10_03_213549_create_students_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+
+            $table->string('address');
+
+            $table->string('mobile');
             $table->timestamps();
         });
     }
