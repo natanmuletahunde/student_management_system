f59563a8eb5c19d93859b4fef7f3eba0d56ddb89
pivnati <natanmuleta911@gmail.com>
2024-10-04 11:01:05 -0700

    modfiy the teacher table

--- database/migrations/2024_10_04_175911_create_teachers_table.php
+++ database/migrations/2024_10_04_175911_create_teachers_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('teachers', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->string('mobile');
             $table->timestamps();
         });
     }
