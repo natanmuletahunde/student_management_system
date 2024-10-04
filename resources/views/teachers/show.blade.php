9206daf4871aac3370978ec2976a758df6b28c06
pivnati <natanmuleta911@gmail.com>
2024-10-04 10:54:07 -0700

    create the teachers link in the web.php

--- resources/views/teachers/show.blade.php
+++ resources/views/teachers/show.blade.php
@@ -3,14 +3,14 @@
  
  
 <div class="card">
-  <div class="card-header">Students Page</div>
+  <div class="card-header">Teachers Page</div>
   <div class="card-body">
    
  
         <div class="card-body">
-        <h5 class="card-title">Name : {{ $students->name }}</h5>
-        <p class="card-text">Address : {{ $students->address }}</p>
-        <p class="card-text">Mobile : {{ $students->mobile }}</p>
+        <h5 class="card-title">Name : {{ $teachers->name }}</h5>
+        <p class="card-text">Address : {{ $teachers->address }}</p>
+        <p class="card-text">Mobile : {{ $teachers->mobile }}</p>
   </div>
        
     </hr>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\TeacherController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('layout');
 });
 Route::resource('/students',StudentController::class);
+Route::resource('/teachers',TeacherController::class);
+
 
 
