851fdaf3333ab8ad57ba970dd2e5e396b792e388
pivnati <natanmuleta911@gmail.com>
2024-10-04 12:03:31 -0700

    modify the courses tabel

--- database/migrations/2024_10_04_190117_create_courses_table.php
+++ database/migrations/2024_10_04_190117_create_courses_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('syllabus');
+            $table->string('duration');
             $table->timestamps();
         });
     }
