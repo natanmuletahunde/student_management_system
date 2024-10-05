ba5ac636457f452afef82184fe02deed780b792f
pivnati <natanmuleta911@gmail.com>
2024-10-05 10:45:33 -0700

    modify the show.blade.php of batches fodler

--- resources/views/batches/show.blade.php
+++ resources/views/batches/show.blade.php
@@ -3,14 +3,14 @@
  
  
 <div class="card">
-  <div class="card-header"> Course Page</div>
+  <div class="card-header">Batches</div>
   <div class="card-body">
    
  
         <div class="card-body">
-        <h5 class="card-title">Name : {{ $courses->name }}</h5>
-        <p class="card-text">Syllabus : {{ $courses->syllabus }}</p>
-        <p class="card-text">Duration : {{ $courses->duration }}</p>
+        <h5 class="card-title">Name : {{ $batches->name }}</h5>
+        <p class="card-text">Syllabus : {{ $batches->course_id }}</p>
+        <p class="card-text">Duration : {{ $batches->start_date }}</p>
   </div>
        
     </hr>
