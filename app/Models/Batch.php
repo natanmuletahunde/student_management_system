3e94cda010c16950ca222e1854a1cb262c258fdb
pivnati <natanmuleta911@gmail.com>
2024-10-05 09:44:22 -0700

    modify the batches model

--- app/Models/Batch.php
+++ app/Models/Batch.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Batch extends Model
-{
+{ 
+    protected $table = 'batches';
+    protected $primaryKey = 'id';
+    protected $fillable = ['name', 'course_id', 'start_date'];
     use HasFactory;
 }
