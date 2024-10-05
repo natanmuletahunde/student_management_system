9145f607eab6c2f99dbe82c5d508018b1f1cf9bb
pivnati <natanmuleta911@gmail.com>
2024-10-05 10:10:50 -0700

    create edit function in the batchcontroller

--- app/Http/Controllers/BatchController.php
+++ app/Http/Controllers/BatchController.php
@@ -49,7 +49,8 @@ class BatchController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $batches = Batch::find($id);
+        return view('batches.edit')->with('batches', $batches);
     }
 
     /**
