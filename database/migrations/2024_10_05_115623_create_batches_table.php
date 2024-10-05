60acbb918a8757ca52ec258db82eda2dea77bf75
pivnati <natanmuleta911@gmail.com>
2024-10-05 05:56:47 -0700

    in the batches table create different types of items

--- database/migrations/2024_10_05_115623_create_batches_table.php
+++ database/migrations/2024_10_05_115623_create_batches_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('batches', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('course_id');
+            $table->date('start_date');
+            $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
             $table->timestamps();
         });
     }
