e0b2e0a0e7ab1f2fff55a0bb62c5340ceac81e86
pivnati <natanmuleta911@gmail.com>
2024-10-05 10:37:27 -0700

    modify the create of the batches

--- resources/views/batches/create.blade.php
+++ resources/views/batches/create.blade.php
@@ -2,17 +2,17 @@
 @section('content')
  
 <div class="card">
-  <div class="card-header">CoursePage</div>
+  <div class="card-header">Batches</div>
   <div class="card-body">
       
-      <form action="{{ url('courses') }}" method="post">
+      <form action="{{ url('batches') }}" method="post">
         {!! csrf_field() !!}
-        <label>Name</label></br>
+        <label>Batch Name</label></br>
         <input type="text" name="name" id="name" class="form-control"></br>
-        <label>Syllabus</label></br>
-        <input type="text" name="syllabus" id="syllabus" class="form-control"></br>
-        <label>Duration</label></br>
-        <input type="text" name="duration" id="duration" class="form-control"></br>
+        <label>Course Name</label></br>
+        <input type="text" name="syllabus" id="course_id" class="form-control"></br>
+        <label>Start Date</label></br>
+        <input type="text" name="duration" id="start_date" class="form-control"></br>
         <input type="submit" value="Save" class="btn btn-success"></br>
     </form>
    
