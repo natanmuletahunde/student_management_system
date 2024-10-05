90abba377851b0abb5fe612c9addeffabe3be15b
pivnati <natanmuleta911@gmail.com>
2024-10-05 15:36:48 -0700

    create  the of enrollment

--- resources/views/batches/create.blade.php
+++ resources/views/batches/create.blade.php
@@ -5,16 +5,16 @@
   <div class="card-header">Batches</div>
   <div class="card-body">
       
-      <form action="{{ url('batches') }}" method="post">
-        {!! csrf_field() !!}
-        <label>Batch Name</label></br>
-        <input type="text" name="name" id="name" class="form-control"></br>
-        <label>Course Name</label></br>
-        <input type="text" name="course_id" id="course_id" class="form-control"></br>
-        <label>Start Date</label></br>
-        <input type="text" name="start_date" id="start_date" class="form-control"></br>
-        <input type="submit" value="Save" class="btn btn-success"></br>
-    </form>
+  <form action="{{ url('batches') }}" method="post">
+    {!! csrf_field() !!}
+    <label>Batch Name</label></br>
+    <input type="text" name="name" id="name" class="form-control"></br>
+    <label>Course Name</label></br>
+    <input type="text" name="course_id" id="course_id" class="form-control"></br>
+    <label>Start Date</label></br>
+    <input type="date" name="start_date" id="start_date" class="form-control"></br> <!-- Change input type to 'date' -->
+    <input type="submit" value="Save" class="btn btn-success"></br>
+</form>
    
   </div>
 </div>
