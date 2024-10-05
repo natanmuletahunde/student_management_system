b39e282f6ce74d56fbb87233b295f2d04fc44040
pivnati <natanmuleta911@gmail.com>
2024-10-05 04:32:53 -0700

    modify the index.blade.php

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -2,10 +2,10 @@
 @section('content') 
                 <div class="card">
                     <div class="card-header">
-                        <h2>Student Application</h2>
+                        <h2>Courses Application</h2>
                     </div>
                     <div class="card-body">
-                        <a href="{{ url('/students/create') }}" class="btn btn-success btn-sm" title="Add New Student">
+                        <a href="{{ url('/courses/create') }}" class="btn btn-success btn-sm" title="Add New Course">
                             <i class="fa fa-plus" aria-hidden="true"></i> Add New
                         </a>
                         <br/>
@@ -16,35 +16,35 @@
                                     <tr>
                                         <th>#</th>
                                         <th>Name</th>
-                                        <th>Address</th>
-                                        <th>Mobile</th>
+                                        <th>syllabus</th>
+                                        <th>duration</th>
                                         <th>Actions</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                @foreach($students as $item)
+                                @foreach($courses as $item)
                                     <tr>
                                         <td>{{ $loop->iteration }}</td>
                                         <td>{{ $item->name }}</td>
-                                        <td>{{ $item->address }}</td>
-                                        <td>{{ $item->mobile }}</td>
+                                        <td>{{ $item->syllabus }}</td>
+                                        <td>{{ $item->duration }}</td>
  
                                         <td>
-                                            <a href="{{ url('/students/' . $item->id) }}" title="View Student">
+                                            <a href="{{ url('/courses/' . $item->id) }}" title="View Course">
                                                 <button class="btn btn-info btn-sm">
                                                     <i class="fa fa-eye" aria-hidden="true"></i> View
                                                 </button>
                                             </a>
-                                            <a href="{{ url('/students/' . $item->id . '/edit') }}" title="Edit Student">
+                                            <a href="{{ url('/courses/' . $item->id . '/edit') }}" title="Edit Course">
                                                 <button class="btn btn-primary btn-sm">
                                                     <i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit
                                                 </button>
                                             </a>
  
-                                            <form method="POST" action="{{ url('/students/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
+                                            <form method="POST" action="{{ url('/courses/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
                                                 {{ method_field('DELETE') }}
                                                 {{ csrf_field() }}
-                                                <button type="submit" class="btn btn-danger btn-sm" title="Delete Student" onclick="return confirm('Confirm delete?')">
+                                                <button type="submit" class="btn btn-danger btn-sm" title="Delete Course" onclick="return confirm('Confirm delete?')">
                                                     <i class="fa fa-trash-o" aria-hidden="true"></i> Delete
                                                 </button>
                                             </form>
