e1be792e39e949b61ae96ff088287a6e029c93b8
pivnati <natanmuleta911@gmail.com>
2024-10-05 04:26:58 -0700

    edit the create.index.php in the course folder

--- resources/views/courses/create.blade.php
+++ resources/views/courses/create.blade.php
@@ -2,16 +2,16 @@
 @section('content')
  
 <div class="card">
-  <div class="card-header">Students Page</div>
+  <div class="card-header">CoursePage</div>
   <div class="card-body">
       
-      <form action="{{ url('students') }}" method="post">
+      <form action="{{ url('courses') }}" method="post">
         {!! csrf_field() !!}
         <label>Name</label></br>
         <input type="text" name="name" id="name" class="form-control"></br>
-        <label>Address</label></br>
+        <label>Syllabus</label></br>
         <input type="text" name="address" id="address" class="form-control"></br>
-        <label>Mobile</label></br>
+        <label>Duration</label></br>
         <input type="text" name="mobile" id="mobile" class="form-control"></br>
         <input type="submit" value="Save" class="btn btn-success"></br>
     </form>
