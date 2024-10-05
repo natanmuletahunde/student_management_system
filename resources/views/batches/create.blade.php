7ccba78741d181f1e7d378168ff9e9b8b4831669
pivnati <natanmuleta911@gmail.com>
2024-10-05 15:00:50 -0700

    modify the create.blade.php in the batches folder

--- resources/views/batches/create.blade.php
+++ resources/views/batches/create.blade.php
@@ -10,9 +10,9 @@
         <label>Batch Name</label></br>
         <input type="text" name="name" id="name" class="form-control"></br>
         <label>Course Name</label></br>
-        <input type="text" name="syllabus" id="course_id" class="form-control"></br>
+        <input type="text" name="course_id" id="course_id" class="form-control"></br>
         <label>Start Date</label></br>
-        <input type="text" name="duration" id="start_date" class="form-control"></br>
+        <input type="text" name="start_date" id="start_date" class="form-control"></br>
         <input type="submit" value="Save" class="btn btn-success"></br>
     </form>
    
