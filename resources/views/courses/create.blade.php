91cbc8f2a23cbf4b861fcd8cb604d78b59ff752a
pivnati <natanmuleta911@gmail.com>
2024-10-05 04:28:33 -0700

    modify the create.blade.php

--- resources/views/courses/create.blade.php
+++ resources/views/courses/create.blade.php
@@ -10,9 +10,9 @@
         <label>Name</label></br>
         <input type="text" name="name" id="name" class="form-control"></br>
         <label>Syllabus</label></br>
-        <input type="text" name="address" id="address" class="form-control"></br>
+        <input type="text" name="syllabus" id="syllabus" class="form-control"></br>
         <label>Duration</label></br>
-        <input type="text" name="mobile" id="mobile" class="form-control"></br>
+        <input type="text" name="duration" id="duration" class="form-control"></br>
         <input type="submit" value="Save" class="btn btn-success"></br>
     </form>
    
