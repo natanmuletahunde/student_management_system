ffd0e1bf9345e7cee98c071534182d052dc3fd9b
pivnati <natanmuleta911@gmail.com>
2024-10-07 09:26:42 -0700

    modify the enrollment model

--- app/Models/Enrollment.php
+++ app/Models/Enrollment.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Enrollment extends Model
-{
+{ 
+    protected $table = 'enrollments';
+    protected $primaryKey = 'id';
+    protected $fillable = ['name', 'batch_id', 'student_id'];
     use HasFactory;
 }
