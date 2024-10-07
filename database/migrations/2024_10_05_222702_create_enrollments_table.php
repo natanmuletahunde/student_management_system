10ba0c929ab02f4f4a181f6b67121be0045c23d9
pivnati <natanmuleta911@gmail.com>
2024-10-07 09:12:12 -0700

    create the table unsignedbiginteger,date,fee,batch

--- database/migrations/2024_10_05_222702_create_enrollments_table.php
+++ database/migrations/2024_10_05_222702_create_enrollments_table.php
@@ -14,7 +14,11 @@ return new class extends Migration
         Schema::create('enrollments', function (Blueprint $table) {
             $table->id();
             $table->string('enroll_no');
-            $table->unsignedBigInteger('');
+            $table->unsignedBigInteger('batch_id');
+            $table->unsignedBigInteger('student_id');
+            $table->date('join_date');
+            $table->double('fee');
+            $table->foreign('batch_id')->references('id')->on('batches')->onDelete('cascade');
             $table->timestamps();
         });
     }
