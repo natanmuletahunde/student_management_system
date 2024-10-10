4107c64278c4b58fe3e51b16c4dcd60b762de2ec
pivnati <natanmuleta911@gmail.com>
2024-10-10 15:30:45 -0700

    modify the layout and change the interfaces

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,108 +4,157 @@
 <head>
     <title>Contact Laravel 8 CRUD</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>
-       Student Management System</title>
-    </title>
+    <title>Student Management System</title>
     <style>
-       /* The side navigation menu */
-.sidebar {
-  margin: 0;
-  padding: 0;
-  width: 200px;
-  background-color: #f1f1f1;
-  position: fixed;
-  height: 100%;
-  overflow: auto;
-}
+        /* The side navigation menu */
+        .sidebar {
+            margin: 0;
+            padding: 0;
+            width: 200px;
+            background-color: #f1f1f1;
+            position: fixed;
+            height: 100%;
+            overflow: auto;
+        }
 
-/* Sidebar links */
-.sidebar a {
-  display: block;
-  color: black;
-  padding: 16px;
-  text-decoration: none;
-}
+        /* Sidebar links */
+        .sidebar a {
+            display: block;
+            color: black;
+            padding: 16px;
+            text-decoration: none;
+        }
 
-/* Active/current link */
-.sidebar a.active {
-  background-color: #04AA6D;
-  color: white;
-}
+        /* Active/current link */
+        .sidebar a.active {
+            background-color: #04AA6D;
+            color: white;
+        }
 
-/* Links on mouse-over */
-.sidebar a:hover:not(.active) {
-  background-color: #555;
-  color: white;
-}
+        /* Links on mouse-over */
+        .sidebar a:hover:not(.active) {
+            background-color: #555;
+            color: white;
+        }
 
-/* Page content. The value of the margin-left property should match the value of the sidebar's width property */
-div.content {
-  margin-left: 200px;
-  padding: 1px 16px;
-  height: 1000px;
-}
+        /* Page content. The value of the margin-left property should match the value of the sidebar's width property */
+        div.content {
+            margin-left: 200px;
+            padding: 1px 16px;
+            height: 1000px;
+        }
 
-/* On screens that are less than 700px wide, make the sidebar into a topbar */
-@media screen and (max-width: 700px) {
-  .sidebar {
-    width: 100%;
-    height: auto;
-    position: relative;
-  }
-  .sidebar a {float: left;}
-  div.content {margin-left: 0;}
-}
+        /* On screens that are less than 700px wide, make the sidebar into a topbar */
+        @media screen and (max-width: 700px) {
+            .sidebar {
+                width: 100%;
+                height: auto;
+                position: relative;
+            }
 
-/* On screens that are less than 400px, display the bar vertically, instead of horizontally */
-@media screen and (max-width: 400px) {
-  .sidebar a {
-    text-align: center;
-    float: none;
-  }
-} 
+            .sidebar a {
+                float: left;
+            }
 
+            div.content {
+                margin-left: 0;
+            }
+        }
+
+        /* On screens that are less than 400px, display the bar vertically, instead of horizontally */
+        @media screen and (max-width: 400px) {
+            .sidebar a {
+                text-align: center;
+                float: none;
+            }
+        }
     </style>
 </head>
 
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <div class="container-fluid">
-                        <a class="navbar-brand" href="#">
-                            <h2>Student Management Project</h2>
-                        </a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                </nav>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-3">
-                <!-- The sidebar -->
-                <div class="sidebar">
-                    <a class="active" href="#home">Home</a>
-                    <a href="{{ url('/students')}}">Student</a>
-                    <a href="{{ url('/teachers')}}">Teacher</a>
-                    <a href="{{ url('/courses')}}">Courses</a>
-                    <a href="{{ url('/batches')}}">Batches</a>
-                    <a href="#about">Enrollment</a>
-                    <a href="#about">payment</a>
+    <div class="container-fluid">
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <h2>Student Management Project</h2>
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
+        </nav>
+
+        <div class="row">
+            <!-- Sidebar -->
+            <div class="col-md-3">
+                <div class="sidebar">
+                    <a class="active" href="#home">Home</a>
+                    <a href="{{ url('/students') }}">Students</a>
+                    <a href="{{ url('/teachers') }}">Teachers</a>
+                    <a href="{{ url('/courses') }}">Courses</a>
+                    <a href="{{ url('/batches') }}">Batches</a>
+                    <a href="#enrollment">Enrollment</a>
+                    <a href="#payment">Payment</a>
+                </div>
+            </div>
+
+            <!-- Content Section -->
             <div class="col-md-9">
-               
-                @yield('content')
-                
+                <div class="content">
+                    @yield('content')
+                </div>
             </div>
         </div>
 
-    </div>
+        <!-- Footer -->
+        <footer class="bg-light text-center text-lg-start mt-5">
+            <div class="container p-4">
+                <div class="row">
+                    <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+                        <h5 class="text-uppercase">Student Management System</h5>
+                        <p>
+                            Efficient management of student information, teacher data, and course materials.
+                        </p>
+                    </div>
+                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                        <h5 class="text-uppercase">Quick Links</h5>
+                        <ul class="list-unstyled mb-0">
+                            <li>
+                                <a href="#" class="text-dark">About Us</a>
+                            </li>
+                            <li>
+                                <a href="#" class="text-dark">Support</a>
+                            </li>
+                            <li>
+                                <a href="#" class="text-dark">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="text-center p-3 bg-dark text-white">
+                © 2024 Student Management System. All rights reserved.
+            </div>
+        </footer>
     </div>
 
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl0KzLDN2R8n5C7f65b92HNy7RTusAXKdu1PE8BK38T6aNLlMlVFLQRw8RA" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhG81rSl2Pd9cH+uhYpWDAJE8johI62mddflEd9gTixrF6VcvEme9Fyne5O" crossorigin="anonymous"></script>
 </body>
 
 </html>
